e16b0f38f5e655f46dcb64b71e1dd21198f228a0
JC7657 <juan.jpjs@gmail.com>
2020-05-08 02:03:37 -0400

    Finished

--- src/main.py
+++ src/main.py
@@ -7,11 +7,14 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Family
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.config['DEBUG'] = True
+app.config['ENV'] = "development"
+app.config['JSON_SORT_KEYS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
@@ -36,6 +39,59 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/members', methods=['GET'])
+def getFamily():
+    family = Family("Doe")
+    familyMembers = family.get_all_members()
+    luckyNumbers = []
+    for member in familyMembers:
+        luckyNumbers = luckyNumbers + member['lucky_numbers']
+    response_body = {
+        "family_name": family.last_name,
+        "members": familyMembers,
+        "lucky_numbers": luckyNumbers,
+        "sum_of_lucky": sum(luckyNumbers)
+    }
+    return jsonify(response_body), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def getMember(member_id = None):
+    family = Family('Doe')
+    member_id = request.view_args['member_id']
+    member = family.get_member(member_id)
+    return jsonify(member)
+
+@app.route('/member', methods=['POST'])
+def newMember():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    family = Family('Doe')
+    name = request.json.get('name', None)
+    if not name:
+        return({"msg": "Missing name parameter"})
+    age = request.json.get('age', None)
+    if not age: 
+        return({"msg": "Missing age parameter"})
+    lucky_numbers = request.json.get('lucky_numbers', None)
+    if not lucky_numbers:
+        return({"msg": "Missing lucky numbers"})
+    new_member = {
+        "id": family._generateId(),
+        "first_name": name,
+        "age": age,
+        "lucky_numbers": lucky_numbers
+    }
+    family.add_member(new_member)
+    return jsonify(family.get_all_members())
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def deleteMember(member_id = None):
+    family = Family('Doe')
+    member_id = request.view_args['member_id']
+    family.delete_member(member_id)
+    return jsonify(family.get_all_members())
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,17 +1,77 @@
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Family:
+    
+    def __init__(self, last_name):
+        self.last_name = last_name
+        # example list of members
+        self._members = [{
+            "id": 27381008,
+            "first_name": "John",
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]
+        },
+        {
+            "id": 27367877,
+            "first_name": "Jane",
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]    
+        },
+        {
+            "id": 75904477,
+            "first_name": "Jimmy",
+            "age": 5,
+            "lucky_numbers": [1]    
+        }]
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+        # read-only: Use this method to generate random members ID's when adding members into the list
+    def _generateId(self):
+        return random.randint(0, 99999999)
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def add_member(self, member):
+        ## you have to implement this method
+        ## append the member to the list of _members
+        self._members.append(member)
+        return self._members
+        
+
+    def delete_member(self, id):
+        ## you have to implement this method
+        ## loop the list and delete the member with the given id
+        newList = []
+        for member in self._members:
+            if member['id'] != id:
+                newList.append(member)
+        self._members = newList
+        return({"msg": "ok"})
+        
+
+    def update_member(self, id, member):
+        ## you have to implement this method
+        ## loop the list and replace the memeber with the given id
+        newList = []
+        for member in self._members:
+            if member['id'] != id:
+                newList.append.member
+            
+        self._members = newList
+        member['id']: id
+        self._members.append(member)
+        return self._members
+
+    def get_member(self, id):
+        ## you have to implement this method
+        ## loop all the members and return the one with the given id
+        for member in self._members:
+            if member['id'] == id:
+                print("match")
+                return member
+            else:
+                print("not match")
+            
+
+    def get_all_members(self):
+        return self._members
